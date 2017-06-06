eb23fa652debb4e544abddd5cb672c4d55a63c11
rpringst <rob.ringstad@gmail.com>
2017-06-06 17:02:32 -0500

    Problem 18 and 67 were the same problem, just different sizes

--- problem18.py
+++ problem18.py
@@ -1,7 +1,30 @@
+def reduce_and_combine(lower, upper):
+	if len(lower) <= len(upper):
+		print('Error!')
+		return
+	else:
+		new = []
+		for i in range(len(lower)-1):
+			if lower[i] >= lower[i+1]:
+				new.append(lower[i])
+			else:
+				new.append(lower[i+1])
+	return [sum(x) for x in zip(upper, new)]
+		
+
 with open('p018_triangle.txt') as f:
 	data = []
 	for line in f:
 		data.append([int(x) for x in line.split()])
 
+results = list(data)
+
+for i in range(len(data)-1, 0, -1):
+	print(i)
+	results[i-1] = reduce_and_combine(results[i], results[i-1])
+	
 for row in data:
+	print(row)
+	
+for row in results:
 	print(row)
--- problem67.py
+++ problem67.py
@@ -1,7 +1,31 @@
+from timeit import default_timer as timer
+
+def reduce_and_combine(lower, upper):
+	if len(lower) <= len(upper):
+		print('Error!')
+		return
+	else:
+		new = []
+		for i in range(len(lower)-1):
+			if lower[i] >= lower[i+1]:
+				new.append(lower[i])
+			else:
+				new.append(lower[i+1])
+	return [sum(x) for x in zip(upper, new)]
+
 with open('p067_triangle.txt') as f:
 	data = []
 	for line in f:
 		data.append([int(x) for x in line.split()])
 
-for row in data:
-	print(row)
+start = timer()
+
+results = list(data)
+
+for i in range(len(data)-1, 0, -1):
+	results[i-1] = reduce_and_combine(results[i], results[i-1])
+
+end = timer()
+
+print(results[0])
+print('Execution time (seconds): ' + str(end-start))
